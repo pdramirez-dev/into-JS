f5a6a8208d55e66c68dec9caf8ed1579752ac743
Pablo <pdramirez.h@gmail.com>
2022-07-26 23:15:41 -0400

    ejercicio 2

---
+++ ejercicio2/src/index.js
@@ -0,0 +1,11 @@
+let name = "Pablo Enrique Diaz Ramirez";
+let edad = 24;
+let isDev = true;
+let birthDate = new Date(5,3,1998);
+let faborBook = {
+    title: "The Roman Latter",
+    author: "San Pablo",
+    date: new Date(1,1,40),
+    url: null
+}
+
